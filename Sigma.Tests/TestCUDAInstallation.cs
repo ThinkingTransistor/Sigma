ef193d2064bf168e5969432349dbcf0786cd4297
Florian Cäsar <florian.caesar@outlook.com>
2016-10-30 16:49:32 +0100

    Added Assert.Ignore if CUDA is unavailable in unit tests
    Added Assert.Ignore if CUDA is unavailable in unit tests

--- Sigma.Tests/TestCUDAInstallation.cs
+++ Sigma.Tests/TestCUDAInstallation.cs
@@ -2,20 +2,52 @@
 using ManagedCuda;
 using ManagedCuda.CudaBlas;
 using NUnit.Framework;
+using System.Runtime.InteropServices;
 
 namespace Sigma.Tests
 {
 	public class TestCUDAInstallation
 	{
+		static bool cudaInstalled;
+		static bool checkedCudaInstalled;
+
+		public static void AssertIgnoreIfCudaUnavailable()
+		{
+			if (!checkedCudaInstalled)
+			{
+				try
+				{
+					new CudaContext();
+
+					cudaInstalled = true;
+				}
+				catch
+				{
+					cudaInstalled = false;
+				}
+
+				checkedCudaInstalled = true;
+			}
+
+			if (!cudaInstalled)
+			{
+				Assert.Ignore();
+			}
+		}
+
 		[TestCase]
 		public void TestCreateDefaultCUDAContext()
 		{
+			AssertIgnoreIfCudaUnavailable();
+
 			CudaContext context = new CudaContext();
 		}
 
 		[TestCase]
 		public void TestCreateCudaBlas()
 		{
+			AssertIgnoreIfCudaUnavailable();
+
 			CudaBlas cublas = new CudaBlas();
 		}
 	}
