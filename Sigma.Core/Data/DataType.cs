6cd57513b847912c2d349de3782ad52c2b796367
Florian Cäsar <florian.caesar@outlook.com>
2016-10-31 21:03:35 +0100

    Added IDataBuffer, IDataType and default data types (DataType)
    Added IDataBuffer, IDataType and default data types (DataType)

---
+++ Sigma.Core/Data/DataType.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigma.Core.Data
+{
+	/// <summary>
+	/// A data type that can be used for data buffers and mathematical operations. Data types are used to define buffer types to use at runtime.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IDataType<T>
+	{
+		/// <summary>
+		/// The underlying system type of this data type. 
+		/// </summary>
+		System.Type UnderlyingType { get; }
+
+		/// <summary>
+		/// The size of this type in bytes.
+		/// </summary>
+		int SizeBytes { get; }
+
+		/// <summary>
+		/// Creates a generic array with a given length and the underlying type of this data type as type. 
+		/// </summary>
+		/// <param name="length">The length of the array to create.</param>
+		/// <returns>An array with the underlying type of this data type as type and the given length.</returns>
+		T[] CreateArray(int length);
+	}
+
+	public class DataType<T> : IDataType<T>
+	{
+		public static readonly IDataType<double> FLOAT64 = new DataType<double>(8);
+		public static readonly IDataType<float> FLOAT32 = new DataType<float>(4);
+
+		public static readonly IDataType<byte> INT8 = new DataType<byte>(1);
+		public static readonly IDataType<short> INT16 = new DataType<short>(2);
+		public static readonly IDataType<int> INT32 = new DataType<int>(4);
+		public static readonly IDataType<long> INT64 = new DataType<long>(2);
+
+		public int SizeBytes
+		{
+			get
+			{
+				return SizeBytes;
+			}
+
+			private set
+			{
+				SizeBytes = value;
+			}
+		}
+
+		public System.Type UnderlyingType
+		{
+			get;
+		} = typeof(T);
+
+		public T[] CreateArray(int length)
+		{
+			return new T[length];
+		}
+
+		public DataType(int sizeBytes)
+		{
+			this.SizeBytes = sizeBytes;
+		}
+	}
+}
---
+++ Sigma.Core/Data/IDataBuffer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigma.Core.Data
+{
+	/// <summary>
+	/// A data buffer view with data of a certain type. Data buffers can be "stacked" hierarchically, meaning data buffers can represent a buffer view of their underlying data.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IDataBuffer<T> : IEnumerable<T>
+	{
+		/// <summary>
+		/// The number of elements in this data buffer.
+		/// </summary>
+		long Count { get; }
+
+		/// <summary>
+		/// The offset to the underlying root data buffer. 
+		/// </summary>
+		long Offset { get; }
+
+		/// <summary>
+		/// The underlying data type to use (e.g. FLOAT32).
+		/// </summary>
+		IDataType<T> Type { get; }
+
+		/// <summary>
+		/// Get the value at a certain index.
+		/// </summary>
+		/// <param name="index">The index.</param>
+		/// <returns>The value at the given index.</returns>
+		T GetValue(long index);
+
+		/// <summary>
+		/// Get the value at a certain index and attempt to explicitly cast to the given type. 
+		/// </summary>
+		/// <typeparam name="TOther">The type to cast to.</typeparam>
+		/// <param name="index">The index.</param>
+		/// <returns>The value at the given index, cast to given type.</returns>
+		TOther GetValueAs<TOther>(long index);
+
+		/// <summary>
+		/// Get a new data buffer view with values referring to this buffer within a certain range. 
+		/// </summary>
+		/// <param name="startIndex">The start index referring to this buffer (not underlying).</param>
+		/// <param name="length">The length.</param>
+		/// <returns></returns>
+		IDataBuffer<T> GetValues(long startIndex, long length);
+
+		/// <summary>
+		/// Get a new data buffer with the given type, with an underlying COPY of all data within the specified range, and attempt to cast all data to the new type. 
+		/// </summary>
+		/// <param name="startIndex">The start index referring to this buffer (not underlying).</param>
+		/// <param name="length">The length.</param>
+		/// <returns>A new data buffer of the given type with a COPY of the specified range.</returns>
+		IDataBuffer<TOther> GetValuesAs<TOther>(long startIndex, long length);
+
+		/// <summary>
+		/// Set the value at the given index.
+		/// </summary>
+		/// <param name="value">The value.</param>
+		/// <param name="index">The index.</param>
+		void SetValue(T value, long index);
+
+		/// <summary>
+		/// Set values of this data buffer to the given values within the specified bounds.
+		/// </summary>
+		/// <param name="values">The values.</param>
+		/// <param name="sourceStartIndex">Where to start copying in the source array (values).</param>
+		/// <param name="destStartIndex">Where to paste within the destination, RELATIVE to the destination buffer (this data buffer).</param>
+		/// <param name="length">Starting at the source start index, how many elements to copy.</param>
+		void SetValues(T[] values, long sourceStartIndex, long destStartIndex, long length);
+
+		/// <summary>
+		/// Set values of this data buffer to the given values within the specified bounds.
+		/// </summary>
+		/// <param name="buffer">The data buffer to copy the data from.</param>
+		/// <param name="sourceStartIndex">Where to start copying in the source buffer, RELATIVE to the source buffer.</param>
+		/// <param name="destStartIndex">Where to paste within the destination, RELATIVE to the destination buffer (this data buffer).</param>
+		/// <param name="destLength">Starting at the source start index, how many elements to copy.</param>
+		void SetValues(IDataBuffer<T> buffer, long sourceStartIndex, long destStartIndex, long destLength);
+
+		/// <summary>
+		/// Copies this the data buffer, keeping the same underlying data (underlying buffers are not copied).
+		/// </summary>
+		/// <returns>A new data buffer view of the same data.</returns>
+		IDataBuffer<T> Copy();
+
+		/// <summary>
+		/// Get the underlying data buffer.
+		/// </summary>
+		/// <returns>The underlying data buffer.</returns>
+		IDataBuffer<T> GetUnderlyingBuffer();
+
+		/// <summary>
+		/// Get the underlying root data buffer (the buffer at the very top, where the actual data resides).
+		/// </summary>
+		/// <returns>The underlying root data buffer.</returns>
+		IDataBuffer<T> GetUnderlyingRootBuffer();
+	}
+}
