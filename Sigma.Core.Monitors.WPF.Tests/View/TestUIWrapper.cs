ccf1599a6b7e949f79fcd626bd00bea8a684d25d
Michael Plainer <plainer@ymail.com>
2016-11-05 10:35:23 +0100

    Added test cases for UI
    +TestGridSize
    +TestUIWrapper

---
+++ Sigma.Core.Monitors.WPF.Tests/View/TestUIWrapper.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Windows.Controls;
+using MahApps.Metro.Controls;
+using NUnit.Framework;
+using Sigma.Core.Monitors.WPF.View;
+
+namespace Sigma.Core.Monitors.WPF.Tests.View
+{
+	public class TestUIWrapper
+	{
+		[TestCase]
+		public void TestUIWrapperCreation()
+		{
+			Thread fred = new Thread(() =>
+			{
+				TestUIWrapperClass wrapper = new TestUIWrapperClass();
+
+				Assert.IsTrue(wrapper.Content is TestControl);
+
+				wrapper.Content.Test = "hello";
+
+				Assert.AreEqual(wrapper.Content.Test, "hello");
+
+				wrapper.Content = new TestControl() { Content = "world" };
+
+				Assert.AreNotEqual(wrapper.Content.Test, "hello");
+				Assert.AreEqual(wrapper.Content.Test, "world");
+			});
+
+			fred.SetApartmentState(ApartmentState.STA);
+			fred.Priority = ThreadPriority.Highest;
+			fred.Start();
+
+			fred.Join();
+		}
+
+		private class TestControl : ContentControl
+		{
+			public string Test { get; set; }
+		}
+
+		private class TestUIWrapperClass : UIWrapper<TestControl>
+		{
+
+		}
+	}
+}
