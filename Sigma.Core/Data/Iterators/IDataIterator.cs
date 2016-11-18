e57e66816946014878c49b8ce00d891cc4de547f
Florian <florian.caesar@outlook.com>
2016-11-18 13:17:41 +0100

    Expanded IDataIterator
    Expanded IDataIterator

--- Sigma.Core/Data/Iterators/IDataIterator.cs
+++ Sigma.Core/Data/Iterators/IDataIterator.cs
@@ -16,10 +16,22 @@ using System.Threading.Tasks;
 
 namespace Sigma.Core.Data.Iterators
 {
+	/// <summary>
+	/// A data iterator which iterates over a dataset in blocks. 
+	/// Note: The blocks yielded are in the BatchTimeFeatures format, where Batch and Time are one-dimensional.
+	/// </summary>
 	public interface IDataIterator
 	{
+		/// <summary>
+		/// The dataset underlying this data iterator.
+		/// </summary>
 		IDataset UnderlyingDataset { get; }
 
+		/// <summary>
+		/// Yield a block from this data iterator.
+		/// If no more blocks can be yielded, return null. 
+		/// </summary>
+		/// <returns>A block from the underlying dataset.</returns>
 		INDArray Yield();
 	}
 }
