d60990f803a1a4b93bab277dc33365ec822138b7
Florian Cäsar <florian.caesar@outlook.com>
2016-11-20 19:50:24 +0100

    Updated project file
    Updated project file

--- Sigma.Core/Data/Iterators/BaseIterator.cs
+++ Sigma.Core/Data/Iterators/BaseIterator.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/* 
+MIT License
+
+Copyright (c) 2016 Florian Cäsar, Michael Plainer
+
+For full license see LICENSE in the root directory of this project. 
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
