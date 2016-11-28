d0c96f49debe061f0b134031ed955ac15527ce5e
Florian Cäsar <florian.caesar@outlook.com>
2016-11-28 21:00:16 +0100

    Refactored WebUtils.GetProxyFromFileOrDefault with Match to be fancier
    Refactored WebUtils.GetProxyFromFileOrDefault with Match to be fancier

--- Sigma.Core/Utils/WebUtils.cs
+++ Sigma.Core/Utils/WebUtils.cs
@@ -9,6 +9,7 @@ For full license see LICENSE in the root directory of this project.
 using log4net;
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading;
 
@@ -52,26 +53,26 @@ namespace Sigma.Core.Utils
 
 					try
 					{
-						if (key == "address" || key == "proxyaddress")
+						if (Match(key, "address", "proxyaddress"))
 						{
 							address = value.Trim();
 						}
-						else if (key == "port" || key == "proxyport")
+						else if (Match(key, "port", "proxyport"))
 						{
 							port = int.Parse(value.Trim());
 						}
-						else if (key == "user" || key == "username")
+						else if (Match(key, "user", "username"))
 						{
 							username = value.Trim();
 						}
-						else if (key == "pass" || key == "password")
+						else if (Match(key, "pass", "password"))
 						{
 							password = value.Trim();
 						}
 					}
 					catch (Exception ex)
 					{
-						Logger.Warn($"Invalid entry at line {line} in file {filepath}.", ex);	
+						Logger.Warn($"Invalid entry at line {line} in file {filepath}.", ex);
 					}
 				}
 			}
@@ -89,6 +90,11 @@ namespace Sigma.Core.Utils
 			}
 
 			return customProxy;
+		}
+
+		private static bool Match(string actual, params string[] toMatch)
+		{
+			return toMatch.Any(match => actual.Equals(match, StringComparison.OrdinalIgnoreCase));
 		}
 	}
 
