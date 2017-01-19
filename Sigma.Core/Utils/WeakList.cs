ab298d164cbd2f6f1dec9013c99e0def9b1b5d26
Michael Plainer <plainer@ymail.com>
2017-01-19 22:10:29 +0100

    Removed WeakList

--- Sigma.Core/Utils/WeakList.cs
+++ Sigma.Core/Utils/WeakList.cs
@@ -7,116 +7,116 @@ using System.Threading.Tasks;
 
 namespace Sigma.Core.Utils
 {
-	public class WeakList<T> : IList<T>
+	public class WeakList<T> //: IList<T>
 	{
-		private readonly List<WeakReference<T>> _innerList = new List<WeakReference<T>>();
+		//private readonly List<WeakReference<T>> _innerList = new List<WeakReference<T>>();
 
-		/// <summary>Returns an enumerator that iterates through a collection.</summary>
-		/// <returns>An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.</returns>
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
-		}
+		///// <summary>Returns an enumerator that iterates through a collection.</summary>
+		///// <returns>An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.</returns>
+		//IEnumerator IEnumerable.GetEnumerator()
+		//{
+		//	return GetEnumerator();
+		//}
 
-		/// <summary>Returns an enumerator that iterates through the collection.</summary>
-		/// <returns>A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the collection.</returns>
-		public IEnumerator<T> GetEnumerator()
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Returns an enumerator that iterates through the collection.</summary>
+		///// <returns>A <see cref="T:System.Collections.Generic.IEnumerator`1" /> that can be used to iterate through the collection.</returns>
+		//public IEnumerator<T> GetEnumerator()
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1" />.</summary>
-		/// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
-		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.</exception>
-		public void Add(T item)
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1" />.</summary>
+		///// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+		///// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.</exception>
+		//public void Add(T item)
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1" />.</summary>
-		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only. </exception>
-		public void Clear()
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1" />.</summary>
+		///// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only. </exception>
+		//public void Clear()
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Determines whether the <see cref="T:System.Collections.Generic.ICollection`1" /> contains a specific value.</summary>
-		/// <returns>true if <paramref name="item" /> is found in the <see cref="T:System.Collections.Generic.ICollection`1" />; otherwise, false.</returns>
-		/// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
-		public bool Contains(T item)
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Determines whether the <see cref="T:System.Collections.Generic.ICollection`1" /> contains a specific value.</summary>
+		///// <returns>true if <paramref name="item" /> is found in the <see cref="T:System.Collections.Generic.ICollection`1" />; otherwise, false.</returns>
+		///// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+		//public bool Contains(T item)
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1" /> to an <see cref="T:System.Array" />, starting at a particular <see cref="T:System.Array" /> index.</summary>
-		/// <param name="array">The one-dimensional <see cref="T:System.Array" /> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1" />. The <see cref="T:System.Array" /> must have zero-based indexing.</param>
-		/// <param name="arrayIndex">The zero-based index in <paramref name="array" /> at which copying begins.</param>
-		/// <exception cref="T:System.ArgumentNullException">
-		/// <paramref name="array" /> is null.</exception>
-		/// <exception cref="T:System.ArgumentOutOfRangeException">
-		/// <paramref name="arrayIndex" /> is less than 0.</exception>
-		/// <exception cref="T:System.ArgumentException">The number of elements in the source <see cref="T:System.Collections.Generic.ICollection`1" /> is greater than the available space from <paramref name="arrayIndex" /> to the end of the destination <paramref name="array" />.</exception>
-		public void CopyTo(T[] array, int arrayIndex)
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1" /> to an <see cref="T:System.Array" />, starting at a particular <see cref="T:System.Array" /> index.</summary>
+		///// <param name="array">The one-dimensional <see cref="T:System.Array" /> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1" />. The <see cref="T:System.Array" /> must have zero-based indexing.</param>
+		///// <param name="arrayIndex">The zero-based index in <paramref name="array" /> at which copying begins.</param>
+		///// <exception cref="T:System.ArgumentNullException">
+		///// <paramref name="array" /> is null.</exception>
+		///// <exception cref="T:System.ArgumentOutOfRangeException">
+		///// <paramref name="arrayIndex" /> is less than 0.</exception>
+		///// <exception cref="T:System.ArgumentException">The number of elements in the source <see cref="T:System.Collections.Generic.ICollection`1" /> is greater than the available space from <paramref name="arrayIndex" /> to the end of the destination <paramref name="array" />.</exception>
+		//public void CopyTo(T[] array, int arrayIndex)
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1" />.</summary>
-		/// <returns>true if <paramref name="item" /> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1" />; otherwise, false. This method also returns false if <paramref name="item" /> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1" />.</returns>
-		/// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
-		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.</exception>
-		public bool Remove(T item)
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1" />.</summary>
+		///// <returns>true if <paramref name="item" /> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1" />; otherwise, false. This method also returns false if <paramref name="item" /> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1" />.</returns>
+		///// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1" />.</param>
+		///// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.</exception>
+		//public bool Remove(T item)
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.</summary>
-		/// <returns>The number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.</returns>
-		public int Count { get; }
+		///// <summary>Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.</summary>
+		///// <returns>The number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1" />.</returns>
+		//public int Count { get; }
 
-		/// <summary>Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.</summary>
-		/// <returns>true if the <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only; otherwise, false.</returns>
-		public bool IsReadOnly { get; }
+		///// <summary>Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only.</summary>
+		///// <returns>true if the <see cref="T:System.Collections.Generic.ICollection`1" /> is read-only; otherwise, false.</returns>
+		//public bool IsReadOnly { get; }
 
-		/// <summary>Determines the index of a specific item in the <see cref="T:System.Collections.Generic.IList`1" />.</summary>
-		/// <returns>The index of <paramref name="item" /> if found in the list; otherwise, -1.</returns>
-		/// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.IList`1" />.</param>
-		public int IndexOf(T item)
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Determines the index of a specific item in the <see cref="T:System.Collections.Generic.IList`1" />.</summary>
+		///// <returns>The index of <paramref name="item" /> if found in the list; otherwise, -1.</returns>
+		///// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.IList`1" />.</param>
+		//public int IndexOf(T item)
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Inserts an item to the <see cref="T:System.Collections.Generic.IList`1" /> at the specified index.</summary>
-		/// <param name="index">The zero-based index at which <paramref name="item" /> should be inserted.</param>
-		/// <param name="item">The object to insert into the <see cref="T:System.Collections.Generic.IList`1" />.</param>
-		/// <exception cref="T:System.ArgumentOutOfRangeException">
-		/// <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.Generic.IList`1" />.</exception>
-		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.IList`1" /> is read-only.</exception>
-		public void Insert(int index, T item)
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Inserts an item to the <see cref="T:System.Collections.Generic.IList`1" /> at the specified index.</summary>
+		///// <param name="index">The zero-based index at which <paramref name="item" /> should be inserted.</param>
+		///// <param name="item">The object to insert into the <see cref="T:System.Collections.Generic.IList`1" />.</param>
+		///// <exception cref="T:System.ArgumentOutOfRangeException">
+		///// <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.Generic.IList`1" />.</exception>
+		///// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.IList`1" /> is read-only.</exception>
+		//public void Insert(int index, T item)
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Removes the <see cref="T:System.Collections.Generic.IList`1" /> item at the specified index.</summary>
-		/// <param name="index">The zero-based index of the item to remove.</param>
-		/// <exception cref="T:System.ArgumentOutOfRangeException">
-		/// <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.Generic.IList`1" />.</exception>
-		/// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.IList`1" /> is read-only.</exception>
-		public void RemoveAt(int index)
-		{
-			throw new NotImplementedException();
-		}
+		///// <summary>Removes the <see cref="T:System.Collections.Generic.IList`1" /> item at the specified index.</summary>
+		///// <param name="index">The zero-based index of the item to remove.</param>
+		///// <exception cref="T:System.ArgumentOutOfRangeException">
+		///// <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.Generic.IList`1" />.</exception>
+		///// <exception cref="T:System.NotSupportedException">The <see cref="T:System.Collections.Generic.IList`1" /> is read-only.</exception>
+		//public void RemoveAt(int index)
+		//{
+		//	throw new NotImplementedException();
+		//}
 
-		/// <summary>Gets or sets the element at the specified index.</summary>
-		/// <returns>The element at the specified index.</returns>
-		/// <param name="index">The zero-based index of the element to get or set.</param>
-		/// <exception cref="T:System.ArgumentOutOfRangeException">
-		/// <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.Generic.IList`1" />.</exception>
-		/// <exception cref="T:System.NotSupportedException">The property is set and the <see cref="T:System.Collections.Generic.IList`1" /> is read-only.</exception>
-		public T this[int index]
-		{
-			get { throw new NotImplementedException(); }
-			set { throw new NotImplementedException(); }
-		}
+		///// <summary>Gets or sets the element at the specified index.</summary>
+		///// <returns>The element at the specified index.</returns>
+		///// <param name="index">The zero-based index of the element to get or set.</param>
+		///// <exception cref="T:System.ArgumentOutOfRangeException">
+		///// <paramref name="index" /> is not a valid index in the <see cref="T:System.Collections.Generic.IList`1" />.</exception>
+		///// <exception cref="T:System.NotSupportedException">The property is set and the <see cref="T:System.Collections.Generic.IList`1" /> is read-only.</exception>
+		//public T this[int index]
+		//{
+		//	get { throw new NotImplementedException(); }
+		//	set { throw new NotImplementedException(); }
+		//}
 	}
 }
