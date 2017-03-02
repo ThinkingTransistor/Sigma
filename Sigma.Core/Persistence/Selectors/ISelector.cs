5a81e3f9c95ebf5f1f7c40e23baadd93e32a4b60
Florian Cäsar <florian.caesar@outlook.com>
2017-03-02 21:44:08 +0100

    Added ISelector, ITrainerSelector
    Added ISelector, ITrainerSelector

---
+++ Sigma.Core/Persistence/Selectors/ISelector.cs
@@ -0,0 +1,36 @@
+﻿/* 
+MIT License
+
+Copyright (c) 2016-2017 Florian Cäsar, Michael Plainer
+
+For full license see LICENSE in the root directory of this project. 
+*/
+
+namespace Sigma.Core.Persistence.Selectors
+{
+	/// <summary>
+	/// A selector used to selectively keep and discards parts of compatible objects (e.g. get trainer without runtime data to store on disk).
+	/// Note: All selector operations must provide a result object with legal object state (the <see cref="Result"/> object must always be valid and usable).
+	/// </summary>
+	/// <typeparam name="T">The type this selector is selecting from.</typeparam>
+	public interface ISelector<out T>
+	{
+		/// <summary>
+		/// The current result object of type <see cref="T"/>.
+		/// </summary>
+		T Result { get; }
+
+		/// <summary>
+		/// Get the "emptiest" available version of this <see cref="T"/> while still retaining a legal object state for type <see cref="T"/>.
+		/// Note: Any parameters that are fixed per-object (such as unique name) must be retained.
+		/// </summary>
+		/// <returns>An empty version of the object.</returns>
+		ISelector<T> Empty();
+
+		/// <summary>
+		/// Get an uninitialised version of this <see cref="T"/> without any runtime information, but ready to be re-initialised.
+		/// </summary>
+		/// <returns></returns>
+		ISelector<T> Uninitialised();
+	}
+}
---
+++ Sigma.Core/Persistence/Selectors/ITrainerSelector.cs
@@ -0,0 +1,52 @@
+﻿/* 
+MIT License
+
+Copyright (c) 2016-2017 Florian Cäsar, Michael Plainer
+
+For full license see LICENSE in the root directory of this project. 
+*/
+
+using System;
+using Sigma.Core.Training;
+
+namespace Sigma.Core.Persistence.Selectors
+{
+	/// <summary>
+	/// A trainer selector to selectively keep and discard <see cref="ITrainer"/> data.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface ITrainerSelector<T> : ISelector<T> where T : ITrainer
+	{
+		/// <summary>
+		/// Keep a set of trainer components, discard all other components.
+		/// </summary>
+		/// <param name="component">The component(s) to keep.</param>
+		/// <returns>A selector for a new trainer with the given component(s) retained.</returns>
+		ISelector<T> Keep(TrainerComponent component);
+
+		/// <summary>
+		/// Discard specified trainer components.
+		/// </summary>
+		/// <param name="component">The component(s) to discard.</param>
+		/// <returns>A selector for a new trainer with the given component(s) discarded.</returns>
+		ISelector<T> Discard(TrainerComponent component);
+	}
+
+	/// <summary>
+	/// An individual trainer component for <see cref="ITrainer"/> data selection (keep / discard).
+	/// </summary>
+	[Flags]
+	public enum TrainerComponent
+	{
+		None             = 0,
+		Sigma            = 1 << 0,
+		Network          = 1 << 1,
+		Initialisers     = 1 << 2,
+		ValueModifiers   = 1 << 3,
+		Optimiser        = 1 << 4,
+		Operator         = 1 << 5,
+		DataProvider     = 1 << 6,
+		DataIterators    = 1 << 7,
+		Hooks            = 1 << 8,
+	}
+}
