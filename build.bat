d08d264015795f0a4c325a0a71357f80e45baaf4
Michael Plainer <plainer@ymail.com>
2017-03-04 16:53:21 +0100

    Update nuget version

--- build.bat
+++ build.bat
@@ -19,14 +19,16 @@ dir
 echo "========"
 echo %WINDIR%
 
-nuget restore Sigma.sln
+powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe','nuget.exe')"
+
+nuget.exe restore Sigma.sln
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Sigma.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 REM pack Sigma.Core
 
 cd Sigma.Core
-nuget pack "Sigma.Core.csproj" -IncludeReferencedProjects -Prop Platform=x64 -Verbosity detailed -Prop Configuration=%config% -Version %version%
+nuget.exe pack "Sigma.Core.csproj" -IncludeReferencedProjects -Prop Platform=x64 -Verbosity detailed -Prop Configuration=%config% -Version %version%
 
 cd ..
 
@@ -40,4 +42,4 @@ cd build
 powershell -Command "(gc Sigma.Core.Monitors.WPF.targets) -replace '~version~', '%version%' | Out-File Sigma.Core.Monitors.WPF.targets"
 
 cd ..
-nuget pack "Sigma.Core.Monitors.WPF.csproj" -IncludeReferencedProjects -Prop Platform=x64 -Verbosity detailed -Prop Configuration=%config% -Version %version%
+nuget.exe pack "Sigma.Core.Monitors.WPF.csproj" -IncludeReferencedProjects -Prop Platform=x64 -Verbosity detailed -Prop Configuration=%config% -Version %version%
