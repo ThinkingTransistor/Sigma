c4003fd5d56dcb36fc96afa83046a14f563fd7b7
Michael Plainer <plainer@ymail.com>
2017-03-04 17:47:52 +0100

    Fix build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 REM @echo Off
 set config=%1
 if "%config%" == "" (
-   set config=NugetBuild
+   set config=Release
 )
  
 set version=1.0.0
@@ -14,30 +14,39 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+set build="%programfiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe"
+
 powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe','nuget.exe')"
 
 copy nuget.exe Sigma.Core
 copy nuget.exe Sigma.Core.Monitors.WPF
 
-nuget.exe restore Sigma.sln
+REM old: "%build%" Sigma.sln /p:Configuration="%config%" /p:Platform=x64
 
-"%programfiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" Sigma.sln /p:Configuration="%config%" /p:Platform=x64
-
-REM pack Sigma.Core
-
+REM build Sigma.Core
 cd Sigma.Core
-nuget.exe pack "Sigma.Core.csproj" -IncludeReferencedProjects -Prop Platform=x64 -Verbosity detailed -Prop Configuration=%config% -Version %version%
+	
+	nuget.exe restore -SolutionDirectory ../
 
-cd ..
+	%build% Sigma.Core.csproj /p:Configuration="%config%" /p:Platform=x64
 
-REM pack Sigma.Core.Monitors.WPF
+	REM pack Sigma.Core
 
-cd Sigma.Core.Monitors.WPF
+	nuget.exe pack "Sigma.Core.csproj" -IncludeReferencedProjects -Prop Platform=x64 -Verbosity detailed -Prop Configuration=%config% -Version %version%
 
-REM update the targets file
 
-cd build
-powershell -Command "(gc Sigma.Core.Monitors.WPF.targets) -replace '~version~', '%version%' | Out-File Sigma.Core.Monitors.WPF.targets"
+REM build Sigma.Core.Monitors.WPF
+cd ../Sigma.Core.Monitors.WPF
 
-cd ..
-nuget.exe pack "Sigma.Core.Monitors.WPF.csproj" -IncludeReferencedProjects -Prop Platform=x64 -Verbosity detailed -Prop Configuration=%config% -Version %version%
+	REM update the targets file
+	cd build
+	powershell -Command "(gc Sigma.Core.Monitors.WPF.targets) -replace '~version~', '%version%' | Out-File Sigma.Core.Monitors.WPF.targets"
+
+	REM actual build
+	nuget.exe restore -SolutionDirectory ../
+
+	%build% Sigma.Core.Monitors.WPF.csproj /p:Configuration="%config%" /p:Platform=x64
+	
+	REM pack Sigma.Core.Monitors.WPF
+	cd ..
+	nuget.exe pack "Sigma.Core.Monitors.WPF.csproj" -IncludeReferencedProjects -Prop Platform=x64 -Verbosity detailed -Prop Configuration=%config% -Version %version%
