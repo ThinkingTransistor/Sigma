34071f3088994a2083823b37995e7edbfbf7f329
Michael Plainer <plainer@ymail.com>
2017-03-09 17:11:32 +0100

    Fix enabled xml documentation file
    Fix enabled xml documentation file
    Improve documentation

--- Sigma.Core.Monitors.WPF/View/Parameterisation/Defaults/SigmaCheckBox.xaml.cs
+++ Sigma.Core.Monitors.WPF/View/Parameterisation/Defaults/SigmaCheckBox.xaml.cs
@@ -9,7 +9,8 @@ For full license see LICENSE in the root directory of this project.
 namespace Sigma.Core.Monitors.WPF.View.Parameterisation.Defaults
 {
 	/// <summary>
-	/// Interaction logic for SigmaCheckBox.xaml
+	/// A custom checkbox that implements <see cref="IParameterVisualiser"/> and can visualise values from a registry
+	/// (booleans in this case).
 	/// </summary>
 	[ParameterVisualiser(typeof(bool), Priority = ParameterVisualiserAttribute.VisualiserPriority.Lower)]
 	public partial class SigmaCheckBox
