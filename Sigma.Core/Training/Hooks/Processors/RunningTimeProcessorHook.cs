57af519ecd908028462b9538ad41fe47045f7cfb
Florian <florian.caesar@outlook.com>
2017-03-14 12:24:05 +0100

    Added separate running time processor and reporter
    Added separate running time processor and reporter

---
+++ Sigma.Core/Training/Hooks/Processors/RunningTimeProcessorHook.cs
@@ -0,0 +1,89 @@
+﻿/* 
+MIT License
+
+Copyright (c) 2016-2017 Florian Cäsar, Michael Plainer
+
+For full license see LICENSE in the root directory of this project. 
+*/
+
+using Sigma.Core.Utils;
+using System;
+using System.Collections.Generic;
+
+namespace Sigma.Core.Training.Hooks.Processors
+{
+	/// <summary>
+	/// A running time processor hook to calculate last and average time between time scale events.
+	/// </summary>
+	[Serializable]
+	public class RunningTimeProcessorHook : BaseHook
+	{
+		/// <summary>
+		/// Create a hook with a certain time step and a set of required global registry entries. 
+		/// </summary>
+		/// <param name="averageSpan">The interval span to average over.</param>
+		/// <param name="timeScale">The time scale this processor should run on (only individual time scales are supported).</param>
+		public RunningTimeProcessorHook(TimeScale timeScale, int averageSpan = 4) : this(timeScale, averageSpan, $"shared.time_{timeScale}")
+		{
+		}
+
+		/// <summary>
+		/// Create a hook with a certain time step and a set of required global registry entries. 
+		/// </summary>
+		/// <param name="timeScale">The time scale.</param>
+		/// <param name="averageSpan">The interval span to average over.</param>
+		/// <param name="sharedResultBaseKey">The shared result base key (under which results will be available).</param>
+		public RunningTimeProcessorHook(TimeScale timeScale, int averageSpan, string sharedResultBaseKey) : base(Utils.TimeStep.Every(1, timeScale))
+		{
+			if (sharedResultBaseKey == null) throw new ArgumentNullException(nameof(sharedResultBaseKey));
+
+			DefaultTargetMode = TargetMode.Global;
+
+			ParameterRegistry.Set("average_span", averageSpan, typeof(int));
+			ParameterRegistry.Set("shared_result_base_key", sharedResultBaseKey, typeof(string));
+			ParameterRegistry.Set("last_running_times", new LinkedList<long>());
+		}
+
+		/// <summary>
+		/// Invoke this hook with a certain parameter registry if optional conditional criteria are satisfied.
+		/// </summary>
+		/// <param name="registry">The registry containing the required values for this hook's execution.</param>
+		/// <param name="resolver">A helper resolver for complex registry entries (automatically cached).</param>
+		public override void SubInvoke(IRegistry registry, IRegistryResolver resolver)
+		{
+			if (ParameterRegistry.ContainsKey("last_time"))
+			{
+				long lastTime = ParameterRegistry.Get<long>("last_time");
+				long currentTime = Operator.RunningTimeMilliseconds;
+				long elapsedTime = currentTime - lastTime;
+
+				LinkedList<long> lastRunningTimes = ParameterRegistry.Get<LinkedList<long>>("last_running_times");
+				string sharedResultBaseKey = ParameterRegistry.Get<string>("shared_result_base_key");
+				int averageSpan = ParameterRegistry.Get<int>("average_span");
+
+				lastRunningTimes.AddLast(elapsedTime);
+
+				int numberRunningTimes = lastRunningTimes.Count;
+
+				if (numberRunningTimes > averageSpan)
+				{
+					lastRunningTimes.RemoveFirst();
+				}
+
+				long averageTime = 0L;
+
+				foreach (long pastTime in lastRunningTimes)
+				{
+					averageTime += pastTime;
+				}
+
+				averageTime /= numberRunningTimes;
+
+				resolver.ResolveSet(sharedResultBaseKey + "_last", elapsedTime, addIdentifierIfNotExists: true);
+				resolver.ResolveSet(sharedResultBaseKey + "_average", averageTime, addIdentifierIfNotExists: true);
+			}
+
+			ParameterRegistry["last_time"] = Operator.RunningTimeMilliseconds;
+		}
+	}
+}
---
+++ Sigma.Core/Training/Hooks/Reporters/RunningTimeReporter.cs
@@ -0,0 +1,57 @@
+﻿/* 
+MIT License
+
+Copyright (c) 2016-2017 Florian Cäsar, Michael Plainer
+
+For full license see LICENSE in the root directory of this project. 
+*/
+
+using log4net;
+using Sigma.Core.Training.Hooks.Processors;
+using Sigma.Core.Utils;
+using System;
+
+namespace Sigma.Core.Training.Hooks.Reporters
+{
+	[Serializable]
+	public class RunningTimeReporter : BaseHook
+	{
+		[NonSerialized]
+		private readonly ILog _logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		/// <summary>
+		/// Create a hook with a certain time step and a set of required global registry entries. 
+		/// </summary>
+		/// <param name="timeStep">The time step.</param>
+		/// <param name="averageSpan">The interval span to average over.</param>
+		public RunningTimeReporter(TimeStep timeStep, int averageSpan = 4) : base(Utils.TimeStep.Every(1, timeStep.TimeScale))
+		{
+			DefaultTargetMode = TargetMode.Global;
+
+			string baseResultKey = $"shared.time_{timeStep.TimeScale}";
+			RequireHook(new RunningTimeProcessorHook(timeStep.TimeScale, averageSpan, baseResultKey));
+
+			ParameterRegistry.Set("base_result_key", baseResultKey, typeof(string));
+		}
+
+		/// <summary>
+		/// Invoke this hook with a certain parameter registry if optional conditional criteria are satisfied.
+		/// </summary>
+		/// <param name="registry">The registry containing the required values for this hook's execution.</param>
+		/// <param name="resolver">A helper resolver for complex registry entries (automatically cached).</param>
+		public override void SubInvoke(IRegistry registry, IRegistryResolver resolver)
+		{
+			string baseResultKey = ParameterRegistry.Get<string>("base_result_key");
+
+			long lastTime = resolver.ResolveGetSingleWithDefault<long>(baseResultKey + "_time", -1L);
+			long averageTime = resolver.ResolveGetSingleWithDefault<long>(baseResultKey + "_average", -1L);
+
+			Report(TimeStep.TimeScale, lastTime, averageTime, lastTime / 1000.0, averageTime / 1000.0);
+		}
+
+		protected virtual void Report(TimeScale timeScale, long lastTime, long averageTime, double lastTimeSeconds, double averageTimeSeconds)
+		{
+			_logger.Info($"Time per {timeScale}: {averageTimeSeconds:N} seconds");
+		}
+	}
+}
