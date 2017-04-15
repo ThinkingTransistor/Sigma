d2976ce294d76c66bead921ebb75dff347d4d1ee
Michael Plainer <plainer@ymail.com>
2017-04-15 21:53:28 +0200

    Add base classes

---
+++ Sigma.Core.Monitors.WPF/View/Graphing/GraphPanel.cs
@@ -0,0 +1,13 @@
+﻿using Sigma.Core.Monitors.WPF.Panels;
+
+namespace Sigma.Core.Monitors.WPF.View.Graphing
+{
+	// maybe directly <NetworkView>
+	public class GraphPanel : GenericPanel<GraphView>
+	{
+		public GraphPanel(string title, object headerContent = null) : base(title, headerContent)
+		{
+			Content = new GraphView();
+		}
+	}
+}
---
+++ Sigma.Core.Monitors.WPF/View/Graphing/GraphView.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Sigma.Core.Monitors.WPF.View.Graphing
+{
+	// may not be required
+	// extend from other class but only if necessary (e.g. NetworkView)
+	public class GraphView : UIElement
+	{
+		// keep empty constructor
+	}
+}
