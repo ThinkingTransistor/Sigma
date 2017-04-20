79947eb5f2a7f371f39342dc523acec0acc0d05f
Michael Plainer <plainer@ymail.com>
2017-04-20 22:46:55 +0200

    Add hacked numpanel
    Add (ugly hacked) numpanel that displays the number for a drawpanel

--- Sigma.Core.Monitors.WPF/Panels/Controls/DrawPanel.cs
+++ Sigma.Core.Monitors.WPF/Panels/Controls/DrawPanel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Controls;
 using Sigma.Core.Architecture;
 using Sigma.Core.Handlers;
 using Sigma.Core.MathAbstract;
@@ -11,11 +12,16 @@ using Sigma.Core.Utils;
 
 namespace Sigma.Core.Monitors.WPF.Panels.Controls
 {
-	public class DrawPanel : GenericPanel<DrawCanvas>, IInputPanel, IDisposable
+	//TODO: move to own class
+	public class NumberPanel : GenericPanel<TextBox>, IOutputPanel
 	{
+		public ITrainer Trainer { get; }
+
+		protected INDArray Values { get; private set; }
+
 		public IComputationHandler Handler { get; set; }
 
-		public INDArray Values { get; private set; }
+		public IInputPanel Input { get; set; }
 
 		/// <summary>
 		///     Create a SigmaPanel with a given title.
@@ -24,9 +30,118 @@ namespace Sigma.Core.Monitors.WPF.Panels.Controls
 		/// <param name="title">The given tile.</param>
 		/// <param name="headerContent">The content for the header. If <c>null</c> is passed,
 		/// the title will be used.</param>
-		public DrawPanel(string title, ITrainer trainer, int drawWidth, int drawHeight, int drawSize, object headerContent = null) : base(title, headerContent)
+		public NumberPanel(string title, ITrainer trainer, object headerContent = null) : base(title, headerContent)
 		{
 			Handler = trainer.Operator.Handler;
+
+			//TODO: style
+			Content = new TextBox();
+
+			Content.FontSize = 72;
+			Content.Text = "?";
+			Trainer = trainer;
+		}
+
+		public void SetInputReference(INDArray values)
+		{
+			//TODO: check if hook already added, remove if...
+			IDictionary<string, INDArray> block = new Dictionary<string, INDArray>();
+			block.Add("inputs", Values);
+			block.Add("targets", Handler.NDArray(1, 1, 10));
+			Trainer.AddGlobalHook(new PassNetworkHook(this, block));
+		}
+
+		public void SetOutput(INDArray output)
+		{
+			output = Handler.SoftMax(output);
+
+			double max = double.NegativeInfinity;
+			int maxNum = -1;
+			Console.WriteLine(output);
+			for (int i = 0; i < 10; i++)
+			{
+				double current = output.GetValue<double>(i);
+				if (current > max)
+				{
+					max = current;
+					maxNum = i;
+				}
+			}
+
+			Content.Text = maxNum.ToString();
+		}
+
+		private class PassNetworkHook : BaseHook
+		{
+			private const string DataIdentifier = "data";
+			private const string PanelIdentifier = "panel";
+			/// <summary>
+			/// Create a hook with a certain time step and a set of required global registry entries. 
+			/// </summary>
+			public PassNetworkHook(IOutputPanel panel, IDictionary<string, INDArray> block) : base(Core.Utils.TimeStep.Every(1, TimeScale.Iteration), "network.self")
+			{
+				ParameterRegistry[DataIdentifier] = block;
+				ParameterRegistry[PanelIdentifier] = panel;
+				InvokeInBackground = true;
+			}
+
+			/// <summary>
+			/// Invoke this hook with a certain parameter registry if optional conditional criteria are satisfied.
+			/// </summary>
+			/// <param name="registry">The registry containing the required values for this hook's execution.</param>
+			/// <param name="resolver">A helper resolver for complex registry entries (automatically cached).</param>
+			public override void SubInvoke(IRegistry registry, IRegistryResolver resolver)
+			{
+				IDictionary<string, INDArray> block = (IDictionary<string, INDArray>) ParameterRegistry[DataIdentifier];
+				IOutputPanel panel = (IOutputPanel) ParameterRegistry[PanelIdentifier];
+
+
+				INetwork network = resolver.ResolveGetSingle<INetwork>("network.self");
+
+				IDataProvider provider = new DefaultDataProvider();
+				provider.SetExternalOutputLink("external_default", (targetsRegistry, layer, targetBlock) =>
+				{
+					panel.SetOutput((INDArray) targetsRegistry["activations"]);
+				});
+
+				DataProviderUtils.ProvideExternalInputData(provider, network, block);
+				network.Run(Operator.Handler, trainingPass: false);
+				DataProviderUtils.ProvideExternalOutputData(provider, network, block);
+			}
+		}
+	}
+
+	public class DrawPanel : GenericPanel<DrawCanvas>, IInputPanel, IDisposable
+	{
+		private IOutputPanel _outputPanel;
+		public IComputationHandler Handler { get; set; }
+
+		public INDArray Values { get; }
+
+		public IOutputPanel OutputPanel
+		{
+			get { return _outputPanel; }
+			set
+			{
+				_outputPanel = value;
+				if (_outputPanel != null)
+				{
+					_outputPanel.Input = this;
+				}
+			}
+		}
+
+		/// <summary>
+		///     Create a SigmaPanel with a given title.
+		///     If a title is not sufficient modify <see cref="SigmaPanel.Header" />.
+		/// </summary>
+		/// <param name="title">The given tile.</param>
+		/// <param name="headerContent">The content for the header. If <c>null</c> is passed,
+		/// the title will be used.</param>
+		public DrawPanel(string title, ITrainer trainer, int drawWidth, int drawHeight, int drawSize, IOutputPanel outputPanel = null, object headerContent = null) : base(title, headerContent)
+		{
+			Handler = trainer.Operator.Handler;
+			OutputPanel = outputPanel;
 
 			Content = new DrawCanvas
 			{
@@ -42,12 +157,6 @@ namespace Sigma.Core.Monitors.WPF.Panels.Controls
 			Values = Handler.NDArray(newVals, 1, 1, oldVals.GetLength(0), oldVals.GetLength(1));
 
 			Content.InputChangedEvent += UpdateValues;
-
-			IDictionary<string, INDArray> block = new Dictionary<string, INDArray>();
-			block.Add("inputs", Values);
-			block.Add("targets", Handler.NDArray(1, 1, 10));
-
-			trainer.AddGlobalHook(new PassNetworkHook(block));
 		}
 
 		private double[] DrawCanvasValuesSingle(DrawCanvas canvas)
@@ -73,41 +182,8 @@ namespace Sigma.Core.Monitors.WPF.Panels.Controls
 				{
 					Values.SetValue(newVals[i], NDArrayUtils.GetIndices(i, Values.Shape, Values.Strides));
 				}
-			}
-		}
 
-		private class PassNetworkHook : BaseHook
-		{
-			private const string DataIdentifier = "data";
-			/// <summary>
-			/// Create a hook with a certain time step and a set of required global registry entries. 
-			/// </summary>
-			public PassNetworkHook(IDictionary<string, INDArray> block) : base(Core.Utils.TimeStep.Every(1, TimeScale.Iteration), "network.self")
-			{
-				ParameterRegistry[DataIdentifier] = block;
-				InvokeInBackground = true;
-			}
-
-			/// <summary>
-			/// Invoke this hook with a certain parameter registry if optional conditional criteria are satisfied.
-			/// </summary>
-			/// <param name="registry">The registry containing the required values for this hook's execution.</param>
-			/// <param name="resolver">A helper resolver for complex registry entries (automatically cached).</param>
-			public override void SubInvoke(IRegistry registry, IRegistryResolver resolver)
-			{
-				IDictionary<string, INDArray> block = (IDictionary<string, INDArray>) ParameterRegistry[DataIdentifier];
-
-				INetwork network = resolver.ResolveGetSingle<INetwork>("network.self");
-
-				IDataProvider provider = new DefaultDataProvider();
-				provider.SetExternalOutputLink("external_default", (targetsRegistry, layer, targetBlock) =>
-				{
-					Console.WriteLine(targetsRegistry["activations"]);
-				});
-
-				DataProviderUtils.ProvideExternalInputData(provider, network, block);
-				network.Run(Operator.Handler, trainingPass: false);
-				DataProviderUtils.ProvideExternalOutputData(provider, network, block);
+				OutputPanel?.UpdateValues(Values);
 			}
 		}
 
